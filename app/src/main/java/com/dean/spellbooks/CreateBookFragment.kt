1d0c11510311a6fd5a42c7adf21b0f3bd319b1bb
Mr-Chunky <dean.carter98@yahoo.com>
2022-05-28 15:27:11 +0200

    chore(additions): Add stats to AccountDetailsFragment.kt and refactor, add spinners to layouts, add two functions to database

--- app/src/main/java/com/dean/spellbooks/CreateBookFragment.kt
+++ app/src/main/java/com/dean/spellbooks/CreateBookFragment.kt
@@ -5,27 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.navArgs
 
-class CreateBookFragment : Fragment(), View.OnClickListener {
+class CreateBookFragment : Fragment(), View.OnClickListener, AdapterView.OnItemSelectedListener {
 
     private var navigationController: NavController? = null
 
     private lateinit var etCreateBookTitle: EditText
     private lateinit var etCreateBookAuthor: EditText
     private lateinit var etCreateBookPages: EditText
-    private lateinit var etCreateBookGenre: EditText
+    private lateinit var spinCreateBookGenre: Spinner
     private lateinit var etCreateBookPublisher: EditText
     private lateinit var etCreateBookYearPublished: EditText
     private lateinit var etCreateBookISBN: EditText
     private lateinit var etCreateBookStarRating: EditText
     private lateinit var btnCancelCreateBook: Button
     private lateinit var btnSaveCreateBook: Button
+    private lateinit var genres: Array<out String>
+    private lateinit var spinnerText: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,13 +42,15 @@ class CreateBookFragment : Fragment(), View.OnClickListener {
         initialiseUIElements(view)
 
         setUpButtonClickListeners()
+
+        setUpGenreSpinner()
     }
 
     private fun initialiseUIElements(view: View) {
         etCreateBookTitle = view.findViewById(R.id.etCreateBookTitle)
         etCreateBookAuthor = view.findViewById(R.id.etCreateBookAuthor)
         etCreateBookPages = view.findViewById(R.id.etCreateBookPages)
-        etCreateBookGenre = view.findViewById(R.id.etCreateBookGenre)
+        spinCreateBookGenre = view.findViewById(R.id.spinCreateBookGenre)
         etCreateBookPublisher = view.findViewById(R.id.etCreateBookPublisher)
         etCreateBookYearPublished = view.findViewById(R.id.etCreateBookYearPublished)
         etCreateBookISBN = view.findViewById(R.id.etCreateBookISBN)
@@ -63,10 +64,18 @@ class CreateBookFragment : Fragment(), View.OnClickListener {
         btnSaveCreateBook.setOnClickListener(this)
     }
 
+    private fun setUpGenreSpinner() {
+        genres = resources.getStringArray(R.array.book_genres)
+        val spinnerAdapter: ArrayAdapter<CharSequence> = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, genres)
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinCreateBookGenre.adapter = spinnerAdapter
+        spinCreateBookGenre.onItemSelectedListener = this
+    }
+
     private fun addBook(view: View) {
         if (checkEmpty(etCreateBookTitle) || checkEmpty(etCreateBookAuthor) ||
             (checkEmpty(etCreateBookPages) || !isNumeric(etCreateBookPages.text.toString().trim())) ||
-            checkEmpty(etCreateBookGenre) || checkEmpty(etCreateBookPublisher) ||
+            spinnerText.isEmpty() || checkEmpty(etCreateBookPublisher) ||
             (checkEmpty(etCreateBookYearPublished) || !isNumeric(etCreateBookYearPublished.text.toString().trim())) ||
             checkEmpty(etCreateBookISBN) ||
             (checkEmpty(etCreateBookStarRating) || !isNumeric(etCreateBookStarRating.text.toString().trim()))) {
@@ -76,7 +85,7 @@ class CreateBookFragment : Fragment(), View.OnClickListener {
             val bookTitle: String = etCreateBookTitle.text.toString().trim()
             val bookAuthor: String = etCreateBookAuthor.text.toString().trim()
             val bookPages: Int = etCreateBookPages.text.toString().trim().toInt()
-            val bookGenre: String = etCreateBookGenre.text.toString().trim()
+            val bookGenre: String = spinnerText
             val bookPublisher: String = etCreateBookPublisher.text.toString().trim()
             val bookYearPublished: Int = etCreateBookYearPublished.text.toString().trim().toInt()
             val ISBN: String = etCreateBookISBN.text.toString().trim()
@@ -91,7 +100,6 @@ class CreateBookFragment : Fragment(), View.OnClickListener {
                 etCreateBookTitle.text.clear()
                 etCreateBookAuthor.text.clear()
                 etCreateBookPages.text.clear()
-                etCreateBookGenre.text.clear()
                 etCreateBookPublisher.text.clear()
                 etCreateBookYearPublished.text.clear()
                 etCreateBookISBN.text.clear()
@@ -123,5 +131,13 @@ class CreateBookFragment : Fragment(), View.OnClickListener {
 
     private fun isNumeric(string: String): Boolean {
         return string.all { char -> char.isDigit() }
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        spinnerText = genres[p2].toString()
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        spinnerText = ""
     }
 }
